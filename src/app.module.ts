dbc646f06cd5845b40c064186644939e7e2857aa
Peter Salomonsen <pjsalomonsen@gmail.com>
2018-01-14 07:44:46 +0100

    Show @angular/material tabs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatTabsModule
     ],
     declarations: [
         AppComponent
